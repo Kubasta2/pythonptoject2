b5f6e957d26d928038941c53ec1cdcc93de93444
Uczeń <Ucze@ZSO1_1>
2023-09-27 12:25:22 +0200

    dijrpo

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+liczba1 = 10
+print(type(liczba1))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+tekst1 = 'informatyka'
+print(type(tekst1))
+
+log1 = True
+print(type(log1))
+
+liczba1 = 'rkprkfkomf'
+print(type(liczba1))
+
+liczba2 = 3.14
+print(type(liczba2))
+
+zbior = {1,1,2,2,4,5}
+print(zbior)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('hi')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
